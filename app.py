a6c40b61e067364ee04e9b4432df2c04ea17319f
abu-samer <125936788+abu-samer@users.noreply.github.com>
2025-07-12 21:07:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template, jsonify
+import csv
 
 app = Flask(__name__)
+
+def read_news():
+    news = []
+    with open('news.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            news.append(row['message'])
+    return news
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/api/news')
-def get_news():
-    return jsonify([
-        "هذا خبر تجريبي 🔥",
-        "خبر ثاني 🚀",
-        "خبر ثالث 🎉"
-    ])
+def api_news():
+    news = read_news()
+    return jsonify(news)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
